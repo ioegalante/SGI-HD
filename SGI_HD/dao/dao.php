abc1aa7df1849edcb9f86b73b3b0b63508d5e5c4
ioegalante <ioelgala@gmail.com>
2020-09-24 23:44:49 -0300

    aa

--- SGI_HD/dao/dao.php
+++ SGI_HD/dao/dao.php
@@ -1,9 +1,11 @@
 <?php
-$serverName = "PC-Ioe\sqlexpress"; //serverName\instanceName
+$serverName = "localhost"; //serverName\instanceName
+
 
 // Puesto que no se han especificado UID ni PWD en el array  $connectionInfo,
 // La conexión se intentará utilizando la autenticación Windows.
 $connectionInfo = array( "Database"=>"sgihd");
+
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
 
 if( $conn ) {
